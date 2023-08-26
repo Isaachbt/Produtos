65f891274004bf807eccd84cc1b7d4e296ddc167
Isaac <isaac.silva1478@gmail.com>
2023-08-26 00:02:41 -0300

    ProtudoService created and created save

---
+++ src/main/java/com/produtos/Produtos/service/ProdutoService.java
@@ -0,0 +1,19 @@
+package com.produtos.Produtos.service;
+
+import com.produtos.Produtos.model.ProdutoModel;
+import com.produtos.Produtos.repositorio.ProdutosRepositorio;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProdutoService {
+
+    private ProdutosRepositorio produtosRepositorio;
+
+
+    @Transactional
+    public ProdutoModel save(ProdutoModel produtoModel)
+    {
+        return produtosRepositorio.save(produtoModel);
+    }
+}
