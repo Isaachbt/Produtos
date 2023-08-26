f8dcea564462b3039680d907f73b017cd5b74c6e
Isaac <isaac.silva1478@gmail.com>
2023-08-25 23:50:35 -0300

    ProtudoModel created

---
+++ src/main/java/com/produtos/Produtos/model/ProdutoModel.java
@@ -0,0 +1,32 @@
+package com.produtos.Produtos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUTOS")
+@Getter
+@Setter
+public class ProdutoModel extends RepresentationModel<ProdutoModel> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue()
+    private UUID idProduto;
+    private String nome;
+    private String dataValidade;
+    private BigDecimal value;
+    private int quantidade;
+}
