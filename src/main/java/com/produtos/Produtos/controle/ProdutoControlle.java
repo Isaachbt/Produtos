a8ed5884b6ac35d39dcfb8d691c193ed7f172f51
Isaac <isaac.silva1478@gmail.com>
2023-08-26 00:06:04 -0300

    ProtudoControlle created

---
+++ src/main/java/com/produtos/Produtos/controle/ProdutoControlle.java
@@ -0,0 +1,10 @@
+package com.produtos.Produtos.controle;
+
+import com.produtos.Produtos.service.ProdutoService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProdutoControlle {
+
+    private ProdutoService produtoService;
+}
--- src/main/java/com/produtos/Produtos/service/ProdutoService.java
+++ src/main/java/com/produtos/Produtos/service/ProdutoService.java
@@ -10,7 +10,6 @@ public class ProdutoService {
 
     private ProdutosRepositorio produtosRepositorio;
 
-
     @Transactional
     public ProdutoModel save(ProdutoModel produtoModel)
     {
