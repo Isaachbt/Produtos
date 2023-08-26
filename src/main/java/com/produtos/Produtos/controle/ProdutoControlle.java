ce0ee1a9be9e1ce70bc8bd70c980c7bea21aac3a
Isaac <isaac.silva1478@gmail.com>
2023-08-26 01:13:36 -0300

    created metodo post in ProdutoControlle with success

--- src/main/java/com/produtos/Produtos/controle/ProdutoControlle.java
+++ src/main/java/com/produtos/Produtos/controle/ProdutoControlle.java
@@ -1,13 +1,30 @@
 package com.produtos.Produtos.controle;
 
+import com.produtos.Produtos.dtos.ProdutoDto;
+import com.produtos.Produtos.model.ProdutoModel;
 import com.produtos.Produtos.service.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ProdutoControlle {
 
+    @Autowired
     private ProdutoService produtoService;
 
 
+    @PostMapping("/produto")
+    public ResponseEntity<ProdutoModel> saveProduto(@Valid @RequestBody ProdutoDto produtoDto)
+    {
+        var produtoModel = new ProdutoModel();
+        BeanUtils.copyProperties(produtoDto,produtoModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.save(produtoModel));
+    }
 
 }
